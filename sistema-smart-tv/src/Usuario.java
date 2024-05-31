b83b6b06ef58bcbfe1fd26001d8f9ced1079f721
elmoguilherme <elmogcss@gmail.com>
2024-05-31 00:12:09 -0300

    praticando

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -9,23 +9,27 @@ public class Usuario {
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
 
-        System.out.println("Canal Atual :" +smartTv.canal);
+        System.out.println("Canal Atual: " +smartTv.canal);
 
         smartTv.mudarCanal(13);
 
-        System.out.println("Canal Atual :" +smartTv.canal);
-        
-        System.out.println("Volume Atual : " +smartTv.volume);
+        System.out.println("Canal Atual: " +smartTv.canal);
 
-        System.out.println("TV Ligada ?" + smartTv.ligada);
+        smartTv.mudarCanal(22);
+
+        System.out.println("Canal Atual: " +smartTv.canal);
         
-        System.out.println("Volume Atual :" +smartTv.volume);
+        System.out.println("Volume Atual:  " +smartTv.volume);
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        
+        System.out.println("Volume Atual: " +smartTv.volume);
 
         smartTv.ligar();
-        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+        System.out.println("TV Ligada? " + smartTv.ligada);
 
         smartTv.desligar();
-        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+        System.out.println("TV Ligada? " + smartTv.ligada);
 
 
     }
